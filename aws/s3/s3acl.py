ac7c23035b2beb68c5f3db9da4cf85a918f38dbe
chenxu.wang <chenxu.wang@veeva.com>
2020-10-14 10:39:26 +0800

    correct bucket acl to private

--- aws/s3/s3acl.py
+++ aws/s3/s3acl.py
@@ -1,18 +1,22 @@
 import boto3
 
 s3_client = boto3.client('s3')
-s3_resource = boto3.resource('s3')
 s3 = boto3.resource('s3')
 
-# bucket = s3.Bucket('mysite599.com')
-# bucket.Acl().put(ACL='public-read')
-
-# [{'Grantee': {'ID': 'af152757608fdf5aee625cdf1d190570b44f601073e0e1a598db5595a3dc9dcc',
-#               'Type': 'CanonicalUser'}, 'Permission': 'FULL_CONTROL'},
-#  {'Grantee': {'Type': 'Group', 'URI': 'http://acs.amazonaws.com/groups/global/AllUsers'},
-#   'Permission': 'READ'}]
 
 def set_bucket_public_to_private():
+    """
+    Set bucket acl to public-read
+    bucket = s3.Bucket('mysite599.com')
+    bucket.Acl().put(ACL='public-read')
+
+    Example of public-read bucket Acl:
+    [{'Grantee': {'ID': 'af152757608fdf5aee625cdf1d190570b44f601073e0e1a598db5595a3dc9dcc',
+                  'Type': 'CanonicalUser'}, 'Permission': 'FULL_CONTROL'},
+     {'Grantee': {'Type': 'Group', 'URI': 'http://acs.amazonaws.com/groups/global/AllUsers'},
+      'Permission': 'READ'}]
+
+    """
     all_users = 'http://acs.amazonaws.com/groups/global/AllUsers'
     bucket = s3.Bucket('mysite599.com')
     bucketacl = s3_client.get_bucket_acl(Bucket='mysite599.com')
@@ -27,4 +31,3 @@ def set_bucket_public_to_private():
         print("current bucket '{}' acl setting is {}".format('mysite599.com', bucketacl['Grants']))
 
 set_bucket_public_to_private()
-
